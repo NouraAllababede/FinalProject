aae9fb70e154cf15f9e474f197a99f4e61c014bd
NouraAllababede <nouraallababede@gmail.com>
2025-06-25 23:44:30 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,17 +1,5 @@
 <?php
-
-// إعداد Laravel للعمل في بيئة Serverless على Vercel
-
-require DIR . '/../vendor/autoload.php';
-
-$app = require_once DIR . '/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$request = Illuminate\Http\Request::capture();
-$response = $kernel->handle($request);
-
-$response->send();
-$kernel->terminate($request, $response);
+// هذا الملف هو نقطة الدخول لتشغيل Laravel من Vercel
+require __DIR__ . '/../public/index.php';
 
  
